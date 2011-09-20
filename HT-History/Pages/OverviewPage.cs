d6f5b46adc59811a2000a17d4d36a945fb92b3af
manuel.barbe@googlemail.com <manuel.barbe@googlemail.com@c1102fbf-b0d3-60e4-611c-ebab95f7796e>
2011-09-20 20:25:49 +0000

    some changes

--- HT-History/Pages/OverviewPage.cs
+++ HT-History/Pages/OverviewPage.cs
@@ -45,79 +45,140 @@ namespace HtHistory.Pages
                 new ColumnHeader() { Text = "First", TextAlign = HorizontalAlignment.Left, Width = 80 },
                 new ColumnHeader() { Text = "Last", TextAlign = HorizontalAlignment.Left, Width = 80 } });
 
-            sortableListViewOverview.SetSorter(1, UserControls.SortableListView.TagSorter<int>());
-            sortableListViewOverview.SetSorter(2, UserControls.SortableListView.TagSorter<int>());
-            sortableListViewOverview.SetSorter(3, UserControls.SortableListView.TagSorter<int>());
-            sortableListViewOverview.SetSorter(4, UserControls.SortableListView.TagSorter<int>());
-            sortableListViewOverview.SetSorter(5, UserControls.SortableListView.TagSorter<int>());
-            sortableListViewOverview.SetSorter(6, UserControls.SortableListView.TagSorter<int>());
-            sortableListViewOverview.SetSorter(7, UserControls.SortableListView.TagSorter<int>());
-            sortableListViewOverview.SetSorter(8, UserControls.SortableListView.TagSorter<int>());
-            sortableListViewOverview.SetSorter(9, UserControls.SortableListView.TagSorter<int>());
-            sortableListViewOverview.SetSorter(10, UserControls.SortableListView.TagSorter<int>());
-            sortableListViewOverview.SetSorter(11, UserControls.SortableListView.TagSorter<int>());
-            sortableListViewOverview.SetSorter(12, UserControls.SortableListView.TagSorter<int>());
-            sortableListViewOverview.SetSorter(13, UserControls.SortableListView.TagSorter<DateTime>());
-            sortableListViewOverview.SetSorter(14, UserControls.SortableListView.TagSorter<DateTime>());
+            sortableListViewOverview
+                .SetSorter(1, UserControls.SortableListView.TagSorter<int>())
+                .SetSorter(2, UserControls.SortableListView.TagSorter<int>())
+                .SetSorter(3, UserControls.SortableListView.TagSorter<int>())
+                .SetSorter(4, UserControls.SortableListView.TagSorter<int>())
+                .SetSorter(5, UserControls.SortableListView.TagSorter<int>())
+                .SetSorter(6, UserControls.SortableListView.TagSorter<int>())
+                .SetSorter(7, UserControls.SortableListView.TagSorter<int>())
+                .SetSorter(8, UserControls.SortableListView.TagSorter<int>())
+                .SetSorter(9, UserControls.SortableListView.TagSorter<int>())
+                .SetSorter(10, UserControls.SortableListView.TagSorter<int>())
+                .SetSorter(11, UserControls.SortableListView.TagSorter<int>())
+                .SetSorter(12, UserControls.SortableListView.TagSorter<int>())
+                .SetSorter(13, UserControls.SortableListView.TagSorter<DateTime>())
+                .SetSorter(14, UserControls.SortableListView.TagSorter<DateTime>());
 
-            //this.sortableListViewOverview.SelectedIndexChanged += OverviewSelectedIndexChanged;
+            this.sortableListViewOverview.SelectedIndexChanged += OverviewSelectedIndexChanged;
 
 
-            //this.sortableListViewDetails.Columns.AddRange(new ColumnHeader[] {
-            //    new ColumnHeader() { Text = "Date", TextAlign = HorizontalAlignment.Left, Width = 80 },
-            //    new ColumnHeader() { Text = "Week", TextAlign = HorizontalAlignment.Left, Width = 50 },
-            //    new ColumnHeader() { Text = "Type", TextAlign = HorizontalAlignment.Left, Width = 150 },  
-            //    new ColumnHeader() { Text = "Match", TextAlign = HorizontalAlignment.Left, Width = 220 },
-            //    new ColumnHeader() { Text = "Position", TextAlign = HorizontalAlignment.Left, Width = 50 },
-            //    new ColumnHeader() { Text = "Minutes", TextAlign = HorizontalAlignment.Center, Width = 50 }});
+            this.sortableListViewDetails.Columns.AddRange(new ColumnHeader[] {
+                new ColumnHeader() { Text = "Name", TextAlign = HorizontalAlignment.Left, Width = 225 },
+                new ColumnHeader() { Text = "TM", TextAlign = HorizontalAlignment.Center, Width = 40 },
+                new ColumnHeader() { Text = "TG", TextAlign = HorizontalAlignment.Center, Width = 40 },
+                new ColumnHeader() { Text = "LM", TextAlign = HorizontalAlignment.Center, Width = 40 },
+                new ColumnHeader() { Text = "LG", TextAlign = HorizontalAlignment.Center, Width = 40 },
+                new ColumnHeader() { Text = "CM", TextAlign = HorizontalAlignment.Center, Width = 40 },
+                new ColumnHeader() { Text = "CG", TextAlign = HorizontalAlignment.Center, Width = 40 },
+                new ColumnHeader() { Text = "QM", TextAlign = HorizontalAlignment.Center, Width = 40 },
+                new ColumnHeader() { Text = "QG", TextAlign = HorizontalAlignment.Center, Width = 40 },
+                new ColumnHeader() { Text = "FM", TextAlign = HorizontalAlignment.Center, Width = 40 },
+                new ColumnHeader() { Text = "FG", TextAlign = HorizontalAlignment.Center, Width = 40 },
+                new ColumnHeader() { Text = "OM", TextAlign = HorizontalAlignment.Center, Width = 40 },
+                new ColumnHeader() { Text = "OG", TextAlign = HorizontalAlignment.Center, Width = 40 },
+                new ColumnHeader() { Text = "First", TextAlign = HorizontalAlignment.Left, Width = 80 },
+                new ColumnHeader() { Text = "Last", TextAlign = HorizontalAlignment.Left, Width = 80 } });
 
-            //sortableListViewDetails.SetSorter(0, UserControls.SortableListView.TagSorter<DateTime>());
-            //sortableListViewDetails.SetSorter(1, UserControls.SortableListView.NullSorter);
-            //sortableListViewDetails.SetSorter(5, UserControls.SortableListView.TagSorter<int>());            
+            sortableListViewDetails
+                .SetSorter(1, UserControls.SortableListView.TagSorter<int>())
+                .SetSorter(2, UserControls.SortableListView.TagSorter<int>())
+                .SetSorter(3, UserControls.SortableListView.TagSorter<int>())
+                .SetSorter(4, UserControls.SortableListView.TagSorter<int>())
+                .SetSorter(5, UserControls.SortableListView.TagSorter<int>())
+                .SetSorter(6, UserControls.SortableListView.TagSorter<int>())
+                .SetSorter(7, UserControls.SortableListView.TagSorter<int>())
+                .SetSorter(8, UserControls.SortableListView.TagSorter<int>())
+                .SetSorter(9, UserControls.SortableListView.TagSorter<int>())
+                .SetSorter(10, UserControls.SortableListView.TagSorter<int>())
+                .SetSorter(11, UserControls.SortableListView.TagSorter<int>())
+                .SetSorter(12, UserControls.SortableListView.TagSorter<int>())
+                .SetSorter(13, UserControls.SortableListView.TagSorter<DateTime>())
+                .SetSorter(14, UserControls.SortableListView.TagSorter<DateTime>());
         }
 
         public void OverviewSelectedIndexChanged(object sender, EventArgs e)
         {
-            //try
-            //{
-            //    sortableListViewDetails.Items.Clear();
+            try
+            {
+                sortableListViewDetails.Items.Clear();
 
-            //    if (sortableListViewOverview.SelectedItems.Count == 0) return;
+                if (sortableListViewOverview.SelectedItems.Count == 0) return;
 
-            //    PlayerStatisticItem<TotalMatches.AppearanceInfo> sitem = sortableListViewOverview.SelectedItems[0].Tag as PlayerStatisticItem<TotalMatches.AppearanceInfo>;
+                PlayerStatisticItem<MatchAppearance> sitem = sortableListViewOverview.SelectedItems[0].Tag as PlayerStatisticItem<MatchAppearance>;
 
-            //    if (sitem == null) return;
+                if (sitem == null) return;
 
-            //    foreach (TotalMatches.AppearanceInfo d in sitem.TotalItems)
-            //    {
-            //        ListViewItem item = new ListViewItem(d.Match.Date.ToShortDateString());
-            //        item.Tag = d.Match.Date;
-            //        item.SubItems[0].Tag = item.Tag;
+                IDictionary<int, PlayerStatisticItem<MatchAppearance>> appearancesBySeason = new Dictionary<int, PlayerStatisticItem<MatchAppearance>>();
 
-            //        object value = 0;
+                foreach (MatchAppearance d in sitem.TotalItems)
+                {
+                    int season = new HtTime(d.Match.Date).Season;
 
-            //        value = new HtTime(d.Match.Date);
-            //        item.SubItems.Add(new ListViewItem.ListViewSubItem(item, value.ToString()) { Tag = value });
+                    if (!appearancesBySeason.ContainsKey(season))
+                    {
+                        appearancesBySeason.Add(season, new PlayerStatisticItem<MatchAppearance>(d.Player));
+                    }
 
-            //        value = d.Match.Type;
-            //        item.SubItems.Add(new ListViewItem.ListViewSubItem(item, value.ToString()) { Tag = value });
+                    appearancesBySeason[season].Add(d, d.Match.Date, d.Match.Type);
+                }
 
-            //        value = d.Match;
-            //        item.SubItems.Add(new ListViewItem.ListViewSubItem(item, value.ToString()) { Tag = value });
 
-            //        value = d.Role;
-            //        item.SubItems.Add(new ListViewItem.ListViewSubItem(item, value.ToString()) { Tag = value });
+                foreach(var v in appearancesBySeason)
+                {
+                    var m = v.Value;
 
-            //        value = -1;
-            //        item.SubItems.Add(new ListViewItem.ListViewSubItem(item, value.ToString()) { Tag = value });
-                    
-            //        sortableListViewDetails.Items.Add(item);
-            //    }
-            //}
-            //catch (Exception ex)
-            //{
-            //    MessageBox.Show(ex.ToString());
-            //}
+                    ListViewItem item = new ListViewItem(string.Format("Season {0,02}", v.Key));
+                    item.Tag = m;
+                    item.SubItems[0].Tag = item.Tag;
+
+                    object value = 0;
+
+                    value = m.TotalItems.Count;
+                    item.SubItems.Add(new ListViewItem.ListViewSubItem(item, value.ToString()) { Tag = value });
+                    value = m.TotalItems.Sum(ma => ma.Goals.Count);
+                    item.SubItems.Add(new ListViewItem.ListViewSubItem(item, value.ToString()) { Tag = value });
+
+                    value = m.LeagueItems.Count;
+                    item.SubItems.Add(new ListViewItem.ListViewSubItem(item, value.ToString()) { Tag = value });
+                    value = m.LeagueItems.Sum(ma => ma.Goals.Count);
+                    item.SubItems.Add(new ListViewItem.ListViewSubItem(item, value.ToString()) { Tag = value });
+
+                    value = m.CupItems.Count;
+                    item.SubItems.Add(new ListViewItem.ListViewSubItem(item, value.ToString()) { Tag = value });
+                    value = m.CupItems.Sum(ma => ma.Goals.Count);
+                    item.SubItems.Add(new ListViewItem.ListViewSubItem(item, value.ToString()) { Tag = value });
+
+                    value = m.QualifierItems.Count;
+                    item.SubItems.Add(new ListViewItem.ListViewSubItem(item, value.ToString()) { Tag = value });
+                    value = m.QualifierItems.Sum(ma => ma.Goals.Count);
+                    item.SubItems.Add(new ListViewItem.ListViewSubItem(item, value.ToString()) { Tag = value });
+
+                    value = m.FriendlyItems.Count;
+                    item.SubItems.Add(new ListViewItem.ListViewSubItem(item, value.ToString()) { Tag = value });
+                    value = m.FriendlyItems.Sum(ma => ma.Goals.Count);
+                    item.SubItems.Add(new ListViewItem.ListViewSubItem(item, value.ToString()) { Tag = value });
+
+                    value = m.OtherItems.Count;
+                    item.SubItems.Add(new ListViewItem.ListViewSubItem(item, value.ToString()) { Tag = value });
+                    value = m.OtherItems.Sum(ma => ma.Goals.Count);
+                    item.SubItems.Add(new ListViewItem.ListViewSubItem(item, value.ToString()) { Tag = value });
+
+                    value = m.First;
+                    item.SubItems.Add(new ListViewItem.ListViewSubItem(item, ((DateTime)value).ToShortDateString()) { Tag = value });
+
+                    value = m.Last;
+                    item.SubItems.Add(new ListViewItem.ListViewSubItem(item, ((DateTime)value).ToShortDateString()) { Tag = value });
+
+                    sortableListViewDetails.Items.Add(item);
+                }
+            
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
         }
 
         protected override void DoWork(object sender, DoWorkEventArgs e)
