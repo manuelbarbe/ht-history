0c37df8409f2de99a039172d4c092cd6156a89d7
manuel.barbe@googlemail.com <manuel.barbe@googlemail.com@c1102fbf-b0d3-60e4-611c-ebab95f7796e>
2011-09-24 21:51:38 +0000

    fixed epoch

--- trunk/HtHistory.Core/DataContainers/HtTime.cs
+++ trunk/HtHistory.Core/DataContainers/HtTime.cs
@@ -7,7 +7,7 @@ namespace HtHistory.Core.DataContainers
 {
     public class HtTime
     {
-        private static readonly DateTime HattrickEpoch = new DateTime(1997, 9, 27);
+        private static readonly DateTime HattrickEpoch = new DateTime(1997, 9, 22); //Monday?
 
         public HtTime(DateTime UTC)
         {
