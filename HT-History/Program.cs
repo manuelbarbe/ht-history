9be3c9eea1b9618a96adb962bdbbdc842f2c3ba2
manuel.barbe@googlemail.com <manuel.barbe@googlemail.com@c1102fbf-b0d3-60e4-611c-ebab95f7796e>
2011-09-29 19:52:26 +0000

    added splash screen

--- HT-History/Program.cs
+++ HT-History/Program.cs
@@ -2,11 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace HtHistory
 {
     static class Program
     {
+        static SplashScreen _splashScreen;
+
+        static void ShowSplash()
+        {
+            Application.Run(_splashScreen);
+        }
+
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -15,6 +24,13 @@ namespace HtHistory
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+
+            _splashScreen = new SplashScreen();
+            Thread thread = new Thread(Program.ShowSplash) { IsBackground = true, ApartmentState = ApartmentState.STA };
+            thread.Start();
+            Thread.Sleep(2000);
+            _splashScreen.BeginInvoke((Action) (() => _splashScreen.Close()) );
+
             Application.Run(new Form1());
         }
     }
