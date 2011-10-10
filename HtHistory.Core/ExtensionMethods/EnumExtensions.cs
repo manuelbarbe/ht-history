8d4fe5cbf64ab72f42ed4cce29d37784018fcd0f
manuel.barbe@googlemail.com <manuel.barbe@googlemail.com@c1102fbf-b0d3-60e4-611c-ebab95f7796e>
2011-10-10 20:47:37 +0000

    added Split()

--- HtHistory.Core/ExtensionMethods/EnumExtensions.cs
+++ HtHistory.Core/ExtensionMethods/EnumExtensions.cs
@@ -7,7 +7,10 @@ namespace HtHistory.Core.ExtensionMethods
 {
     public static class EnumExtensions
     {
-        public static IEnumerable<T> SafeEnum<T>(this IEnumerable<T> e) where T : class
+
+        #region SafeEnum
+
+        public static IEnumerable<T> SafeEnum<T>(this IEnumerable<T> e) //where T : class
         {
             if (e != null)
             {
@@ -17,5 +20,35 @@ namespace HtHistory.Core.ExtensionMethods
                 }
             }
         }
+
+        #endregion
+
+
+        #region Split
+
+
+        public static IDictionary<TKey, IEnumerable<TValue>> Split<TKey, TValue>(this IEnumerable<TValue> me, Func<TValue, TKey> keyFromValue) where TKey : IComparable<TKey>
+        {
+            IDictionary<TKey, IList<TValue>> ret = new Dictionary<TKey, IList<TValue>>();
+
+            foreach (TValue v in me.SafeEnum())
+            {
+                TKey key = keyFromValue(v);
+
+                if (!ret.ContainsKey(key))
+                {
+                    ret.Add(key, new List<TValue>());
+                }
+
+                ret[key].Add(v);
+            }
+
+            return (IDictionary<TKey, IEnumerable<TValue>>) ret;
+        }
+
+        #endregion
+
+
+
     }
 }
