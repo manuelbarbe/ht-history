07ea872d8991a7fb21230fb061f2c05a347c755b
manuel.barbe@googlemail.com <manuel.barbe@googlemail.com@c1102fbf-b0d3-60e4-611c-ebab95f7796e>
2011-10-10 20:49:51 +0000

    backup commit

---
+++ trunk/HT-History/Export/TableExporterCSV.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HtHistory.Core.ExtensionMethods;
+
+namespace HtHistory.Export
+{
+    public class TableExporterCSV : ITableExporter
+    {
+        private string _separator;
+
+        public TableExporterCSV(string separator)
+        {
+            if (string.IsNullOrEmpty(separator))
+            {
+                throw new Exception("CSV separator has to contain at least one character");
+            }
+
+            _separator = separator;
+        }
+
+        public void Export(ITable table, System.IO.TextWriter writer)
+        {
+            foreach (object obj in table.ColumHeaders.SafeEnum())
+            {
+                writer.Write(obj.ToString());
+                writer.Write(_separator);
+            }
+            writer.WriteLine();
+
+            foreach (object[] row in table.Data)
+            {
+                foreach (object column in row)
+                {
+                    writer.Write(column.ToString());
+                    writer.Write(_separator);
+                }
+                writer.WriteLine();
+            }
+        }
+    }
+}
