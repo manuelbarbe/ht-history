d4073a502ea1ab332f22c6823b7a9ac43c2c1278
manuel.barbe@googlemail.com <manuel.barbe@googlemail.com@c1102fbf-b0d3-60e4-611c-ebab95f7796e>
2011-11-20 20:48:41 +0000

    see diff

--- HtHistory.Statistics/Players/MatchAppearance.cs
+++ HtHistory.Statistics/Players/MatchAppearance.cs
@@ -12,6 +12,34 @@ namespace HtHistory.Statistics.Players
         public MatchDetails Match { get; set; }
         public Lineup.LineupRole Role { get; set; }
         public IList<Goal> Goals { get; set; }
+        public uint? SubstituteIn { get; set; }
+        public uint? SubstituteOut { get; set; }
+        public uint? YellowCarded { get; set; }
+        public uint? RedCarded { get; set; }
+
+        public uint Minutes
+        {
+            get
+            {
+                try
+                {
+                    checked
+                    {
+                        uint minutes = Match.Minutes;
+
+                        if (SubstituteIn != null) minutes -= SubstituteIn.Value;
+                        if (SubstituteOut != null) minutes -= (Match.Minutes - SubstituteOut.Value);
+                        if (RedCarded != null) minutes -= (Match.Minutes - RedCarded.Value);
+
+                        return minutes;
+                    }
+                }
+                catch
+                {
+                    return 0;
+                }
+            }
+        }
 
         public MatchAppearance(Player player, MatchDetails match, Lineup.LineupRole role)
         {
@@ -20,7 +48,6 @@ namespace HtHistory.Statistics.Players
             Player = player;
             Match = match;
             Role = role;
-
             Goals = new List<Goal>();
         }
     }
