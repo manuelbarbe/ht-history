1f40e9d9b260de75db3e549604b6b32235234522
manuel.barbe@googlemail.com <manuel.barbe@googlemail.com@c1102fbf-b0d3-60e4-611c-ebab95f7796e>
2012-07-01 21:05:39 +0000

    added options start/end date when selecting team id

--- trunk/HT-History/Dialogs/TeamIdDialog.cs
+++ trunk/HT-History/Dialogs/TeamIdDialog.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using HtHistory.Core.ExtensionMethods;
 
 namespace HtHistory.Dialogs
 {
@@ -15,6 +16,11 @@ namespace HtHistory.Dialogs
         {
             InitializeComponent();
             textBoxTeamId.Text = teamId.ToString();
+            dateTimePickerFrom.MaxDate =
+            dateTimePickerTo.MaxDate =
+            dateTimePickerTo.Value =
+            dateTimePickerTo.Value = DateTime.Now.ToHtTime();
+            ShowHideDates(radioButtonPeriod.Checked);
         }
 
         private uint _teamId = 0;
@@ -24,6 +30,34 @@ namespace HtHistory.Dialogs
             get
             {
                 return _teamId;
+            }
+        }
+ 
+        public DateTime? StartDate
+        {
+            get
+            {
+                if (radioButtonPeriod.Checked)
+                {
+                    DateTime dt = dateTimePickerFrom.Value;
+                    return new DateTime(dt.Year, dt.Month, dt.Day, 0, 0, 0);
+                }
+                return null;
+            }
+        }
+
+        public DateTime? EndDate
+        {
+            get
+            {
+                if (radioButtonPeriod.Checked)
+                {
+                    DateTime dt = dateTimePickerTo.Value;
+                    DateTime dt2 = new DateTime(dt.Year, dt.Month, dt.Day, 23, 59, 59);
+                    if (dt2 > dateTimePickerTo.MaxDate) return dateTimePickerTo.MaxDate;
+                    else return dt2;
+                }
+                return null;
             }
         }
 
@@ -38,6 +72,17 @@ namespace HtHistory.Dialogs
                 MessageBox.Show("Cannot parse value. Defaulting to 0.");
                 _teamId = 0;
             }
+        }
+
+        private void ShowHideDates(bool show)
+        {
+            dateTimePickerFrom.Enabled =
+            dateTimePickerTo.Enabled = show;
+        }
+
+        private void radioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            ShowHideDates(radioButtonPeriod.Checked);
         }
 
     }
--- trunk/HT-History/Tasks/GetMatchesTask.cs
+++ trunk/HT-History/Tasks/GetMatchesTask.cs
@@ -13,9 +13,32 @@ namespace HtHistory.Tasks
         private IMatchArchiveBridge _mab;
         private IMatchDetailsBridge _mdb;
         private ITeamDetailsBridge _tdb;
+        private DateTime? _startDate;
+        private DateTime? _endDate;
         private uint _teamId;
 
         private IList<MatchDetails> _mdl = null;
+
+        public GetMatchesTask(  uint teamId,
+                                DateTime? startDate,
+                                DateTime? endDate,
+                                IMatchArchiveBridge mab,
+                                IMatchDetailsBridge mdb)
+        {
+            if (startDate == null) throw new ArgumentNullException("startDate");
+            _startDate = startDate;
+
+            if (endDate == null) throw new ArgumentNullException("endDate");
+            _endDate = endDate;
+
+            if (mab == null) throw new ArgumentNullException("mab");
+            _mab = mab;
+
+            if (mdb == null) throw new ArgumentNullException("mdb");
+            _mdb = mdb;
+
+            _teamId = teamId;
+        }
 
         public GetMatchesTask(uint teamId, ITeamDetailsBridge tdb, IMatchArchiveBridge mab, IMatchDetailsBridge mdb)
         {
@@ -39,15 +62,31 @@ namespace HtHistory.Tasks
             {
                 ReportProgress(0, "Getting general team information");
 
-                TeamDetails teamDetails = _tdb.GetTeamDetails(_teamId);
-                if (teamDetails == null || teamDetails.Owner == null || teamDetails.Owner.JoinDate == null)
+                DateTime? startDate = _startDate;
+                DateTime? endDate = _endDate;
+
+                if (_tdb != null)
                 {
-                    throw new Exception("Cannot get join date of owner");
+                    TeamDetails teamDetails = _tdb.GetTeamDetails(_teamId);
+                    if (teamDetails == null || teamDetails.Owner == null || teamDetails.Owner.JoinDate == null)
+                    {
+                        throw new Exception("Cannot get join date of owner");
+                    }
+
+                    startDate = teamDetails.Owner.JoinDate.Value;
+                    endDate = DateTime.Now.ToHtTime();
                 }
 
+                if (startDate == null || endDate == null)
+                {
+                    throw new Exception("missing start and/or end date");
+                }
+
+                //ReportProgress(10, String.Format("Getting match archive from {0} to {1}", startDate.Value.ToShortDateString(), endDate.Value.ToShortDateString()));
                 ReportProgress(10, "Getting match archive");
 
-                MatchArchive ar = _mab.GetMatches(_teamId, teamDetails.Owner.JoinDate.Value, DateTime.Now.ToHtTime());
+
+                MatchArchive ar = _mab.GetMatches(_teamId, startDate.Value, endDate.Value);
                 int noMatches = ar.Count();
                 int noCurMatch = 0;
 
